3a1aab88ff7ef69da8e3bc60a56ba96805e7509b
weixuan <weixuan75@163.com>
2017-07-20 16:16:28 +0800

    菜单

--- erp/manager/controllers/MenuController.php
+++ erp/manager/controllers/MenuController.php
@@ -15,14 +15,8 @@ class MenuController extends ConfController {
     public $layout="js";
     public function actionIndex(){
         $model = new Menu();
-        var_dump( $model::getMenu(0));
-
-//        $model = Menu::find();
-//        $count = $model->count();
-//        $pageSize = Yii::$app->params['menu']['list'];
-//        $pager = new Pagination(['totalCount' => $count, 'pageSize' => $pageSize]);
-//        $managers = $model->offset($pager->offset)->limit($pager->limit)->all();
-//        return $this->render("index", ['managers' => $managers, 'pager' => $pager]);
+        $managers = $model::getMenu(0);
+        return $this->render("index", ['managers' => $managers]);
     }
     public function actionAdd(){
         $Menu = new Menu();
