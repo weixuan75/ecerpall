2c0189737369ff1e2cbc7942f7292ee99bff73e6
weixuan <weixuan75@163.com>
2017-07-28 14:18:55 +0800

    店铺model

--- erp/index/views/default/index.php
+++ erp/index/views/default/index.php
@@ -1,10 +1,32 @@
-<div class="index-default-index">
-    fasdas
-    df
-    asdf
-    a
-    dsf
-    <?php
-//    var_dump($param);
-    ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="Cache-Control" content="no-siteapp" />
+    <title>H+ 后台主题UI框架 - 主页</title>
+
+    <meta name="keywords" content="H+后台主题,后台bootstrap框架,会员中心主题,后台HTML,响应式后台">
+    <meta name="description" content="H+是一个完全响应式，基于Bootstrap3最新版本开发的扁平化主题，她采用了主流的左右两栏式布局，使用了Html5+CSS3等现代技术">
+
+    <!--[if lt IE 8]>
+    <meta http-equiv="refresh" content="0;ie.html" />
+    <![endif]-->
+
+    <!--    <link rel="shortcut icon" href="favicon.ico">-->
+    <link href="/hplus1/css/bootstrap.min.css?v=3.3.5" rel="stylesheet">
+    <link href="/hplus1/css/font-awesome.min.css?v=4.4.0" rel="stylesheet">
+    <link href="/hplus1/css/animate.min.css" rel="stylesheet">
+    <link href="/hplus1/css/style.min.css?v=4.0.0" rel="stylesheet">
+</head>
+<body class="fixed-sidebar full-height-layout gray-bg" style="overflow:hidden">
+<div class="row wrapper wrapper-content animated flip">
+    <div class="middle-box text-center animated flip">
+            <a href="http://a.com/index.php?r=manager" class="btn btn-lg btn-primary m-t">系统管理后台</a>
+            <a href="http://a.com/index.php?r=shop" class="btn btn-lg btn-primary m-t">店铺管理后台</a>
+    </div>
 </div>
+</body>
+</html>
